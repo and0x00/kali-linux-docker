abc7e2a1a70c03e9dcd7ca7a29825c919ea0a752
Anderson <90927438+and0x00@users.noreply.github.com>
2022-11-07 16:10:53 -0300

    Update start.cmd

--- start.cmd
+++ start.cmd
@@ -1,5 +1,5 @@
 @echo off
-del $home\ssh\known_hosts ::delete X2Go known hosts
+del $home\ssh\known_hosts
 docker build --build-arg PASSWORD=%your_password%  -t and0x00/kali-linux -f Dockerfile .
 docker create --name kali-linux -p 1022:22 -t and0x00/kali-linux
 docker start kali-linux
