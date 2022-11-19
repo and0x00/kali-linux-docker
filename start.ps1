33ffb9dc0ea81b6a5a382794da964c24310df2df
and0x00 <90927438+and0x00@users.noreply.github.com>
2022-11-19 14:58:22 -0300

    Update and rename start.cmd to start.ps1

--- start.ps1
+++ start.ps1
@@ -1,4 +1,3 @@
-@echo off
 del $home\ssh\known_hosts
 docker build --build-arg PASSWORD=%your_password%  -t and0x00/kali-linux -f Dockerfile .
 docker create --name kali-linux -p 1022:22 -t and0x00/kali-linux
